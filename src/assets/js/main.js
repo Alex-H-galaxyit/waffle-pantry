3b6a42419528a2cf423bbc46c42d8e0218ff0b2a
Alex-H-galaxyit <alex.h@galaxy-it.net>
2023-11-15 16:49:36 +0200

    update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,3 +3,14 @@ window.addEventListener("load", async () => {
   document.querySelectorAll(".important_none")?.forEach(($el) => $el.classList.remove("important_none"))
 })
 
+import { Hero } from "./modules/hero"
+import { tabs } from "./modules/tabs"
+
+Hero()
+tabs(
+  ".tabs-kitchen__header",
+  ".tabs-kitchen__header-item",
+  ".tabs-kitchen__content-item",
+  "tabs-kitchen__header-item--active",
+)
+tabs(".tabs__header", ".tabs__header-item", ".tabs__content-item", "tabs__header-item--active")
